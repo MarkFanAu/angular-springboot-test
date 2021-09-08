784817732111b6cfc813bbca9194dadc60dde9d8
MarkFanAu <pkufancy@gmail.com>
2021-09-09 09:28:10 +1000

    first commit

---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { getMaxListeners } from 'process';
+import{ Employee} from '../employee'
+import { EmployeeService } from '../employee.service';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+
+  employees: Employee[];
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit() {
+    this.getEmployee();
+  }
+
+  private getEmployee(){
+     this.employeeService.getEmployeesList().subscribe(data => {
+       this.employees = data;
+     })
+  }
+
+}
